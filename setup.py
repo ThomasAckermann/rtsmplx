6cd5df9063c991ab8dac515f1acf467d8488031a
ThomasAckermann <ackermann.th@outlook.com>
2021-07-10 16:33:46 +0200

    chore: filled setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="rtsmplx",
+    version="1.0",
+    description="Real time version of the SMPL-X model",
+    long_description=long_description,
+    author="Thomas Ackermann",
+    author_email="t.ackermann@stud.uni-heidelberg.de",
+    packages=["rtsmplx"]
+)
