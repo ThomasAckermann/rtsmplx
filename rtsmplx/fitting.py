062ff1b490224bf20fa2aff0d2f0611b05da9a2f
ThomasAckermann <ackermann.th@outlook.com>
2021-08-17 09:48:14 +0200

    chore changed opt structure a little bit

--- rtsmplx/fitting.py
+++ rtsmplx/fitting.py
@@ -8,7 +8,7 @@ import rtsmplx.camera as cam
 import pytorch3d
 
 
-def forward(data, body_model, body=False, face=False, hands=False):
+def opt_step(data, body_model, body=False, face=False, hands=False, lr=lr):
     image = data[0]
     image_landmarks = data[1]
     pose_image_landmarks = image_landmarks.body_landmarks()
@@ -16,10 +16,14 @@ def forward(data, body_model, body=False, face=False, hands=False):
     camera = cam.Camera()
 
     if body == True:
+        betas = body_model.body_pose
+        print(betas)
+        betas.requires_grad = True
         joints = body_model.get_joints()
         pose_prediction = camera.orthographic_projection(joints)
         pose_loss_pred = pose_loss(pose_prediction, pose_image_landmarks)
-
+    else:
+        pose_loss_pred = 0
 
     if face == True:
         bary_coords = body_model.bary_coords
@@ -28,17 +32,22 @@ def forward(data, body_model, body=False, face=False, hands=False):
         transf_bary_coords = transform_bary_coords(bary_coords, bary_vertices)
         face_predictions = camera.orthographic_projection(transf_bary_coords)
         face_loss_pred = face_loss(face_predictions, face_image_landmarks)
+    else:
+        face_loss_pred = 0
 
     if hands == True:
         # Work in progress
         hands_loss_pred = 0
+    else:
+        hands_loss_pred = 0
 
+    opt = optimizer(lr=lr)
+    loss_pred = loss(
+            pose_loss=pose_loss_pred, face_loss=face_loss_pred, hands_loss=hands_loss_pred
+            )
+    loss_pred.backward()
+    print(betas)
     return predictions
-
-
-def backward(loss):
-    loss.backward()
-    return None
 
 
 def pose_loss(joint_coords_2d, landmarks_2d):
