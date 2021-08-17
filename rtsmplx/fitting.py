5578326b94cb94c50575e1bf5c63701650f9d88c
ThomasAckermann <ackermann.th@outlook.com>
2021-08-17 10:22:22 +0200

    fix: bug fixes in opt pipeline

--- rtsmplx/fitting.py
+++ rtsmplx/fitting.py
@@ -8,18 +8,19 @@ import rtsmplx.camera as cam
 import pytorch3d
 
 
-def opt_step(data, body_model, body=False, face=False, hands=False, lr=lr):
+def opt_step(data, body_model, body=False, face=False, hands=False, lr=1e-3):
     image = data[0]
     image_landmarks = data[1]
     pose_image_landmarks = image_landmarks.body_landmarks()
     face_image_landmarks = image_landmarks.face_landmarks()[:17, :]
     camera = cam.Camera()
+    # opt = optimizer(params, lr=lr)
 
     if body == True:
-        betas = body_model.body_pose
-        print(betas)
-        betas.requires_grad = True
-        joints = body_model.get_joints()
+        body_pose_params = body_model.body_pose
+        body_pose_params.requires_grad = True
+        joints = body_model.get_joints(body_pose=body_pose_params)
+        joints = joints[:33, :]
         pose_prediction = camera.orthographic_projection(joints)
         pose_loss_pred = pose_loss(pose_prediction, pose_image_landmarks)
     else:
@@ -41,29 +42,36 @@ def opt_step(data, body_model, body=False, face=False, hands=False, lr=lr):
     else:
         hands_loss_pred = 0
 
-    opt = optimizer(lr=lr)
+    # opt.zero_grad()
     loss_pred = loss(
-            pose_loss=pose_loss_pred, face_loss=face_loss_pred, hands_loss=hands_loss_pred
-            )
+        pose_loss=pose_loss_pred, face_loss=face_loss_pred, hands_loss=hands_loss_pred
+    )
     loss_pred.backward()
-    print(betas)
-    return predictions
+    body_pose_params = body_pose_params - lr * body_pose_params.grad
+    return body_pose_params
 
 
 def pose_loss(joint_coords_2d, landmarks_2d):
-    return nn.MSELoss(bary_coords_2d, landmarks_2d)
+    loss_func = mse_loss()
+    return loss_func(joint_coords_2d, landmarks_2d)
 
 
 def face_loss(bary_coords_2d, landmarks_2d):
     return nn.MSELoss(bary_coords_2d, landmarks_2d)
 
-def loss(pose_loss=0, face_loss=0, hands_loss=0)
+
+def loss(pose_loss=0, face_loss=0, hands_loss=0):
     loss_val = pose_loss + face_loss + hands_loss
     return loss_val
 
+def mse_loss():
+    return nn.MSELoss()
 
+
+"""
 def optimizer(lr=1e-3):
     return optim.LBFGS(lr=lr)
+"""
 
 
 def transform_bary_coords(bary_coords, bary_vertices):
@@ -71,7 +79,5 @@ def transform_bary_coords(bary_coords, bary_vertices):
     return transf_bary_coords
 
 
-
 if __name__ == "__main__":
     pass
-
